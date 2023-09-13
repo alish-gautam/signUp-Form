2a45ff420df294c64fa207368d7c3ba0954c7937
Alish Gautam <alishgautam890@gmail.com>
2023-09-13 06:47:36 +0545

    Sign Up form in the process

---
+++ index.js
@@ -0,0 +1,8 @@
+let pw=document.getElementById("password")
+let confirmpw=document.getElementById("confirmpass")
+let userpass=document.querySelector(".userpassword")
+if(pw.value!=confirmpw.value){
+    let incorrect=document.createElement("h1")
+    incorrect.innerText="Passwords do not match"
+    userpass.appendChild(incorrect)
+}
